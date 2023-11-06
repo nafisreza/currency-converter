5db86ac34a420a2c5fe2977a0d52e139a69da274
nafisreza <bigganinafis@gmail.com>
2023-11-06 10:52:33 +0600

    📌 Custom hook designed for currency info

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+
+  useEffect(() => {
+    let api = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+
+    fetch(api)
+    .then((res) => res.json())
+    .then(() => setData(res[currency]))
+    console.log(data);
+  }, [currency]);
+
+  return data 
+}
+
+export default useCurrencyInfo;
